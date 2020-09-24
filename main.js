83d63756a77a85256a9bbcfb7e6800e0035b04ed
AleksRomanov <kosmonavt-vavilon@mail.ru>
2020-09-24 20:34:11 +0700

    HomeWork 2

--- main.js
+++ main.js
@@ -1,8 +1,121 @@
-alert("Привет Марафонец!");
-console.log("Привет Марафонец!");
+let taskNumber = prompt('Какое задание выбрать 1 или 2?', '2');
 
-const firstName = 'Aleks';
-const lastName = 'Romanov';
+// Задание №1
+let firstRow = 'мама мыла раму';
+let secondRow = 'собака друг человека';
+let letter = 'а';
 
-console.log(firstName)
-console.log(lastName)
+function getRow(firstRow, secondRow) {
+    let firstCounter = 0;
+    let secondCounter = 0;
+
+    for (let i = 0; i <= firstRow.length - 1; i++) {
+
+        if (firstRow.charAt(i) === letter) {
+            ++firstCounter;
+        }
+    }
+    for (let i = 0; i <= secondRow.length - 1; i++) {
+
+        if (secondRow.charAt(i) === letter) {
+            ++secondCounter;
+        }
+    }
+
+
+    if (firstCounter > secondCounter) {
+        return firstRow
+    } else if (firstCounter < secondCounter) {
+        return secondRow
+    } else {
+        return 'Количество букв "' + letter + '" в строках равно'
+    }
+}
+
+function startFirstTask(){
+    firstRow = prompt('Напишите первое словосочетание', firstRow);
+    secondRow = prompt('Напишите второе словосочетание', secondRow);
+    letter = prompt('Введите букву для поиска', letter);
+    console.log(getRow(firstRow, secondRow));
+}
+
+
+// Задание №2
+let phone = '+71234567890';
+
+function formattedPhone(phone) {
+    let result = '';
+    if (phone.length === 12) {
+        for (let i = 0; i <= phone.length - 1; i++) {
+            if (i === 1) {
+                result += phone.charAt(i) + ' (';
+
+            } else if (i === 4) {
+                result += phone.charAt(i) + ') ';
+
+            } else if (i === 7 || i === 9) {
+                result += phone.charAt(i) + '-';
+
+            } else {
+                result += phone.charAt(i);
+            }
+        }
+
+        return result
+    } else if (phone.length === 11) {
+        for (let i = 0; i <= phone.length - 1; i++) {
+            if (i === 0) {
+                result += '+7 (';
+
+            } else if (i === 3  ) {
+                result += phone.charAt(i) + ') ';
+
+            } else if (i === 6 || i === 8) {
+                result += phone.charAt(i) + '-';
+
+            } else {
+                result += phone.charAt(i);
+            }
+        }
+
+        return result
+    } else if (phone.length === 10) {
+        for (let i = 0; i <= phone.length - 1; i++) {
+            if (i === 0) {
+                result += '+7 (' + phone.charAt(i);
+
+            } else if (i === 2) {
+                result += phone.charAt(i) + ') ';
+
+            } else if (i === 5 || i === 7) {
+                result += phone.charAt(i) + '-';
+
+            } else {
+                result += phone.charAt(i);
+            }
+        }
+
+        return result
+    } else {
+        alert('Похоже это не номер телефона, попытайся еще');
+        startSecondTask();
+        return result
+    }
+
+}
+
+function startSecondTask(){
+    phone = prompt('Введите номер телефона', phone);
+    console.log(formattedPhone(phone));
+}
+
+
+
+if (taskNumber === '1') {
+    startFirstTask();
+} else {
+    startSecondTask();
+}
+
+
+
