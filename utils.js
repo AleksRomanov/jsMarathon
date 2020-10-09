e86151443ce373129f778e93f8ba51f3b4bd24fe
AleksRomanov <kosmonavt-vavilon@mail.ru>
2020-10-10 02:08:52 +0700

    HomeWork 8. rework_3

--- utils.js
+++ utils.js
@@ -1,6 +1,3 @@
-// random = (num) => Math.ceil(Math.random() * num)
-
-// function random (num)  {Math.ceil(Math.random() * num)};
 function random(num) {
     return Math.ceil(Math.random() * num);
 }
